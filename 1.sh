9e7f3279a6a83d65473640f465f32b8875256c12
sharathvutpala <sharathchandravutpala@gmail.com>
2016-10-26 11:24:16 +0530

    test

--- 1.sh
+++ 1.sh
@@ -1,6 +1,4 @@
 #! /bin/sh
-<<<<<<< HEAD
-echo hello world, sharath
-=======
-echo hello world, sharath!!
->>>>>>> a8dfdc0cf040459fd1a8256d11236d9ec703a938
+
+echo hello, world!
+echo  jenkins is integrated with github
