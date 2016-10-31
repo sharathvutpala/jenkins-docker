d8ad408204f740d4ea5bc8d2cf6a7f24773fbaf2
sharathvutpala <sharathchandravutpala@gmail.com>
2016-10-31 11:58:43 +0530

    test

--- 1.sh
+++ 1.sh
@@ -1,16 +1,7 @@
 #! /bin/sh
 
 echo hello, world!
-echo jenkins is integrated with github.!!
-echo test
-echo testing again to check PR status
-echo merge before build removed
-echo test
-echo post build action is added
-echo abcd
-echo webhook is tested after restarting server
-echo develop3 branch added
-echo develop2 is updated after creating develop3
-echo PR is created for develop2
-echo new job is added to test PR feature
-echo abcdefghijkl
+
+
+
+
